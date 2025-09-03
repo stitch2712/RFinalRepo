1cec5bf33ce9f775c7e6553f998320fb04b095be
stitch2712 <suzain.ali@emory.edu>
2025-09-03 14:45:47 -0400

    Step 2: Reading the CSV file

--- RfinalProjectScript.R
+++ RfinalProjectScript.R
@@ -3,3 +3,6 @@ library(here)
 library(readr)
 library(gtsummary)
 library(ggplot2)
+
+#Read in a dataset and save a file (can be data, table, figure, etc.):
+data <- read.csv(here::here("/Users/suzain/Downloads/Food.csv"), sep = ",")
